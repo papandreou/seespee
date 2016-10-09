d922da5e5b31d54adf256fb3e81a9ac3e0c826e6
Andreas Lind <andreas@one.com>
2016-10-09 13:28:28 +0200

    Initial commit, release 1.0.0.

---
+++ README.md
@@ -0,0 +1,53 @@
+seespee
+=======
+
+[![NPM version](https://badge.fury.io/js/seespee.svg)](http://badge.fury.io/js/seespee)
+[![Build Status](https://travis-ci.org/papandreou/seespee.svg?branch=master)](https://travis-ci.org/papandreou/seespee)
+[![Coverage Status](https://img.shields.io/coveralls/papandreou/seespee.svg)](https://coveralls.io/r/papandreou/seespee?branch=master)
+[![Dependency Status](https://david-dm.org/papandreou/seespee.svg)](https://david-dm.org/papandreou/seespee)
+
+Generate a `Content-Security-Policy` header for a website. The website is crawled
+for scripts, stylesheets, images, fonts, appcache manifests etc., which will
+be listed by their origin. Inline scripts and stylesheets with be hashed so
+`'unsafe-inline'` can be avoided.
+
+
+```
+$ npm install -g seespee
+$ seespee https://news.ycombinator.com/
+Content-Security-Policy: img-src news.ycombinator.com; style-src https://news.ycombinator.com/news.css?0jKc9Keyn2Zl7D1UAQcy; script-src https://news.ycombinator.com/hn.js?0jKc9Keyn2Zl7D1UAQcy```
+```
+
+It also works with a website located in a directory on a file system:
+
+```
+$ seespee --root /path/to/my/project /path/to/my/project/index.html
+```
+
+If the website has an existing `Content-Security-Policy` in a
+[meta tag](https://www.w3.org/TR/CSP2/#delivery-html-meta-element)
+it will be detected and taken into account so all the existing directives
+are supported. This behavior can be disabled with the `--ignoreexisting`
+parameter.
+
+You can also specify custom directives to include on the command line via
+the `--include` switch. The provided directives will be taken into account
+when adding new ones so you won't end up with redundant entries that are
+already whitelisted by eg. `'self'`:
+
+```
+$ seespee --include "script-src 'self'; object-src 'none'; style-src 'self'" https://news.ycombinator.com/
+Content-Security-Policy: script-src 'self'; object-src 'none'; style-src 'self'; img-src news.ycombinator.com
+```
+
+
+TODO
+====
+
+* Handle HTTP redirects as per https://www.w3.org/TR/CSP2/#source-list-paths-and-redirects
+
+License
+=======
+
+Seespee is licensed under a standard 3-clause BSD license -- see the
+`LICENSE` file for details.
---
+++ test/seespee.js
@@ -0,0 +1,92 @@
+var expect = require('unexpected')
+    .use(require('unexpected-mitm'))
+    .use(require('unexpected-sinon'));
+
+var seespee = require('../lib/seespee');
+
+var AssetGraph = require('assetgraph');
+var sinon = require('sinon');
+
+describe('seespee', function () {
+    var config;
+    var fakeConsole;
+    beforeEach(function () {
+        config = {};
+        config.console = fakeConsole = {
+            log: sinon.spy().named('fakeConsole.log')
+        };
+    });
+
+    it('should populate from an external host', function () {
+        return expect(function () {
+            return new AssetGraph()
+                .loadAssets('http://www.example.com/index.html')
+                .queue(seespee(config));
+        }, 'with http mocked out', [
+            {
+                request: 'GET http://www.example.com/index.html',
+                response: {
+                    headers: {
+                        'Content-Type': 'text/html; charset=utf-8'
+                    },
+                    body:
+                        '<!DOCTYPE html>' +
+                        '<html>' +
+                        '<head><link rel="stylesheet" href="styles.css"></head>' +
+                        '<body><script>alert("foo");</script></body>' +
+                        '</html>'
+                }
+            },
+            {
+                request: 'GET http://www.example.com/styles.css',
+                response: {
+                    headers: {
+                        'Content-Type': 'text/css'
+                    },
+                    body: 'body {color: maroon;}'
+                }
+            }
+        ], 'not to error').then(function () {
+            expect(fakeConsole.log, 'to have calls satisfying', function () {
+                fakeConsole.log("Content-Security-Policy: style-src http://www.example.com/styles.css; script-src 'sha256-bAUA9vTw1GbyqKZp5dovTxTQ+VBAw7L9L6c2ULDtcqI='");
+            });
+        });
+    });
+
+    it('should support an existing policy given as a string', function () {
+        config.include = "script-src 'self'; object-src 'none'";
+        return expect(function () {
+            return new AssetGraph()
+                .loadAssets('http://www.example.com/index.html')
+                .queue(seespee(config));
+        }, 'with http mocked out', [
+            {
+                request: 'GET http://www.example.com/index.html',
+                response: {
+                    headers: {
+                        'Content-Type': 'text/html; charset=utf-8'
+                    },
+                    body:
+                        '<!DOCTYPE html>' +
+                        '<html>' +
+                        '<head><link rel="stylesheet" href="styles.css"></head>' +
+                        '<body><script>alert("foo");</script></body>' +
+                        '</html>'
+                }
+            },
+            {
+                request: 'GET http://www.example.com/styles.css',
+                response: {
+                    headers: {
+                        'Content-Type': 'text/css'
+                    },
+                    body: 'body {color: maroon;}'
+                }
+            }
+        ], 'not to error').then(function () {
+            expect(fakeConsole.log, 'to have calls satisfying', function () {
+                fakeConsole.log("Content-Security-Policy: script-src 'self' 'sha256-bAUA9vTw1GbyqKZp5dovTxTQ+VBAw7L9L6c2ULDtcqI='; object-src 'none'; style-src http://www.example.com/styles.css");
+            });
+        });
+    });
+});
