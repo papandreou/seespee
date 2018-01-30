ceb53e9ef557f3b34d59a81af51d0a8f9edc1c55
Andreas Lind <andreaslindpetersen@gmail.com>
2018-01-31 00:35:26 +0100

    Add --pretty switch (defaults to true)

--- test/reformatCsp.js
+++ test/reformatCsp.js
@@ -5,11 +5,9 @@ describe('reformatCsp', function () {
     it('should make a section for each directive', function () {
         expect(
             reformatCsp(
-                'TheHeader',
                 'foo bar quux; baz yadda;'
             ),
             'to equal',
-            'TheHeader:\n' +
             '  foo bar quux;\n' +
             '  baz yadda;'
         );
@@ -18,12 +16,10 @@ describe('reformatCsp', function () {
     it('should reflow when a line exceeds 80 chars (default maxWidth)', function () {
         expect(
             reformatCsp(
-                'TheHeader',
                 '12345678 000000000 111111111 222222222 333333333 444444444 555555555 666666666 777777777 888888888 ' +
                 '999999999 aaaaaaaaa bbbbbbbbb ccccccccc ddddddddd eeeeeeeee fffffffff'
             ),
             'to equal',
-            'TheHeader:\n' +
             '  12345678 000000000 111111111 222222222 333333333 444444444 555555555 666666666\n' +
             '    777777777 888888888 999999999 aaaaaaaaa bbbbbbbbb ccccccccc ddddddddd\n' +
             '    eeeeeeeee fffffffff;'
@@ -33,12 +29,10 @@ describe('reformatCsp', function () {
     it('should honor a custom maxWidth', function () {
         expect(
             reformatCsp(
-                'TheHeader',
                 '000000000 111111111 222222222 333333333 444444444 555555555 666666666 777777777 888888888 999999999 aaaaaaaaa',
                 { maxWidth: 42 }
             ),
             'to equal',
-            'TheHeader:\n' +
             '  000000000 111111111 222222222 333333333\n' +
             '    444444444 555555555 666666666\n' +
             '    777777777 888888888 999999999\n' +
